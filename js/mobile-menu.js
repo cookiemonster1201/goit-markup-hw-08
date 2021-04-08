8522c47fdb4775e5fcc8f7cb1d778f995707cb6f
Alona Mykytiuk <m.j.aliona@gmail.com>
2021-04-08 22:49:11 +0300

    adapts the rest of the sections

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,13 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    const mobileBtnClose = document.querySelector("[data-menu-close]");
 
     menuBtnRef.addEventListener("click", () => {
-    const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    mobileMenuRef.classList.toggle("menu-container--open");
+    })
 
-    menuBtnRef.classList.toggle("menu-button--open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-
+    mobileBtnClose.addEventListener('click', () => {
     mobileMenuRef.classList.toggle("menu-container--open");
     });
-})();
+})()
